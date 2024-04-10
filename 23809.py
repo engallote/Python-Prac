133ccd27cdf79d5f44c44870f144486fa8b600b7
engallote <53823323+engallote@users.noreply.github.com>
2024-04-11 01:49:45 +0900

    Add files via upload

---
+++ 23809.py
@@ -0,0 +1,46 @@
+import sys
+
+
+N = int(sys.stdin.readline())
+
+
+for _ in range(N):
+    for _ in range(N):
+        print("@", end='')
+    for _ in range(N):
+        print("   ", end='')
+    for _ in range(N):
+        print("@", end='')
+    print()
+
+for _ in range(N):
+    for _ in range(N):
+        print("@", end='')
+    for _ in range(N):
+        print("  ", end='')
+    for _ in range(N):
+        print("@", end='')
+    print()
+
+for _ in range(N):
+    for _ in range(N):
+        print("@@@", end='')
+    print()
+
+for _ in range(N):
+    for _ in range(N):
+        print("@", end='')
+    for _ in range(N):
+        print("  ", end='')
+    for _ in range(N):
+        print("@", end='')
+    print()
+
+for _ in range(N):
+    for _ in range(N):
+        print("@", end='')
+    for _ in range(N):
+        print("   ", end='')
+    for _ in range(N):
+        print("@", end='')
+    print()
