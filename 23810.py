0415730f52821fe60ba6bca3f7173cc76d9b129b
engallote <53823323+engallote@users.noreply.github.com>
2024-04-12 08:43:28 +0900

    Add files via upload

---
+++ 23810.py
@@ -0,0 +1,30 @@
+import sys
+
+
+N = int(sys.stdin.readline())
+
+
+for _ in range(N):
+    for _ in range(N):
+        print("@@@@@", end='')
+    print()
+
+for _ in range(N):
+    for _ in range(N):
+        print("@", end='')
+    print()
+
+for _ in range(N):
+    for _ in range(N):
+        print("@@@@@", end='')
+    print()
+
+for _ in range(N):
+    for _ in range(N):
+        print("@", end='')
+    print()
+
+for _ in range(N):
+    for _ in range(N):
+        print("@", end='')
+    print()
---
+++ 23812.py
@@ -0,0 +1,42 @@
+import sys
+
+
+N = int(sys.stdin.readline())
+
+
+for _ in range(N):
+    for _ in range(N):
+        print("@", end='')
+    for _ in range(N):
+        print("   ", end='')
+    for _ in range(N):
+        print("@", end='')
+    print()
+
+for _ in range(N):
+    for _ in range(N):
+        print("@@@@@", end='')
+    print()
+
+for _ in range(N):
+    for _ in range(N):
+        print("@", end='')
+    for _ in range(N):
+        print("   ", end='')
+    for _ in range(N):
+        print("@", end='')
+    print()
+
+for _ in range(N):
+    for _ in range(N):
+        print("@@@@@", end='')
+    print()
+
+for _ in range(N):
+    for _ in range(N):
+        print("@", end='')
+    for _ in range(N):
+        print("   ", end='')
+    for _ in range(N):
+        print("@", end='')
+    print()
