3ff0f94646ec448701af11fc91cf13e48c25ccd9
engallote <53823323+engallote@users.noreply.github.com>
2025-03-22 01:51:52 +0900

    Add files via upload

---
+++ 33612.py
@@ -0,0 +1,15 @@
+import sys
+
+
+N = int(sys.stdin.readline())
+
+if N == 1:
+    print("2024 8")
+elif N == 2:
+    print("2025 3")
+elif N == 3:
+    print("2025 10")
+elif N == 4:
+    print("2026 5")
+else:
+    print("2026 12")
