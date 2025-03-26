978544307fe0d403f984fec47365d3753487538b
engallote <53823323+engallote@users.noreply.github.com>
2025-03-27 01:50:16 +0900

    Add files via upload

---
+++ 27889.py
@@ -0,0 +1,13 @@
+import sys
+
+
+S = sys.stdin.readline().rstrip()
+
+if "NLCS" == S:
+    print("North London Collegiate School")
+elif "BHA" == S:
+    print("Branksome Hall Asia")
+elif "KIS" == S:
+    print("Korea International School")
+else:
+    print("St. Johnsbury Academy")
