bf7dda5613f46bc1ad1a593f3bb95357661c2cc9
engallote <53823323+engallote@users.noreply.github.com>
2025-03-28 02:27:31 +0900

    Add files via upload

---
+++ 28927.py
@@ -0,0 +1,12 @@
+import sys
+
+
+t1, e1, f1 = map(int, sys.stdin.readline().split())
+t2, e2, f2 = map(int, sys.stdin.readline().split())
+
+if t1 * 3 + e1 * 20 + f1 * 120 > t2 * 3 + e2 * 20 + f2 * 120:
+    print("Max")
+elif t1 * 3 + e1 * 20 + f1 * 120 < t2 * 3 + e2 * 20 + f2 * 120:
+    print("Mel")
+else:
+    print("Draw")
