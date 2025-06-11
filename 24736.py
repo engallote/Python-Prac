adff20fddfcb97a9cf7d4d790f5734c1fd2175c8
engallote <53823323+engallote@users.noreply.github.com>
2025-06-11 23:58:39 +0900

    Add files via upload

---
+++ 24736.py
@@ -0,0 +1,8 @@
+import sys
+
+
+T, F, S, P, C = map(int, sys.stdin.readline().split())
+print(T * 6 + F * 3 + S * 2 + P + C * 2, end=' ')
+
+T, F, S, P, C = map(int, sys.stdin.readline().split())
+print(T * 6 + F * 3 + S * 2 + P + C * 2)
