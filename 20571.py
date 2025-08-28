4939b32842581f47ddc0a5f832e97472cd29d464
engallote <53823323+engallote@users.noreply.github.com>
2025-08-29 02:18:53 +0900

    Add files via upload

---
+++ 20571.py
@@ -0,0 +1,39 @@
+import sys
+
+
+N, M = sys.stdin.readline().rstrip().split()
+floor = int(M)
+
+if N == "residential":
+    if floor == 1:
+        print(0)
+    elif 2 <= floor <= 5:
+        print(1)
+    elif 6 <= floor <= 10:
+        print(2)
+    elif 11 <= floor <= 15:
+        print(3)
+    else:
+        print(4)
+elif N == "industrial":
+    if floor == 1:
+        print(0)
+    elif 2 <= floor <= 4:
+        print(1)
+    elif 5 <= floor <= 8:
+        print(2)
+    elif 9 <= floor <= 12:
+        print(3)
+    elif 13 <= floor <= 16:
+        print(4)
+    else:
+        print(5)
+else:
+    if floor == 1:
+        print(0)
+    elif 2 <= floor <= 7:
+        print(1)
+    elif 8 <= floor <= 14:
+        print(2)
+    else:
+        print(3)
